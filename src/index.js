229c7c8ac59939c94c3380e85d825ef5fc38a2d0
Angel <angeljosealvarezperezz@gmail.com>
2023-08-07 19:01:22 +0200

    Version 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,69 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const fileRoot = document.getElementById("root");
+const root = ReactDOM.createRoot(fileRoot);
+
+const App = () => {
+  const [taskName, setTaskName] = useState("");
+  const [taskDesc, setDescName] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const setHandleName = (event) => {
+    setTaskName(event.target.value);
+  };
+
+  const handleDeleteTask = (index) => {
+    const newTasks = tasks.filter((_, i) => i !== index);
+    setTasks(newTasks);
+  };
+  const setHandleDesc = (event) => {
+    setDescName(event.target.value);
+  };
+
+  const submitInfo = () => {
+    console.log("Nombre: ", taskName);
+    console.log("Descripción: ", taskDesc);
+
+    setTasks([...tasks, { Name: taskName, Desc: taskDesc }]);
+    console.log("Tareas", tasks);
+  };
+  return (
+    <div>
+      <div className="center-elements">
+        <h1>Nombre de la tarea</h1>
+        <input value={taskName} onChange={setHandleName}></input>
+        <br></br>
+        <hr className="horizontal-line"></hr>
+        <h3>Descripción de la tarea</h3>
+        <textarea
+          value={taskDesc}
+          onChange={setHandleDesc}
+          rows="4"
+          cols="50"
+        ></textarea>
+        <br></br>
+        <hr className="horizontal-line"></hr>
+        <button onClick={submitInfo}>Crear tarea</button>
+      </div>
+      {/* Mostar tareas */}
+
+      <div className="task-grid">
+        {tasks.map((task, index) => (
+          <div key={index} className="task-card">
+            <strong>Nombre:</strong> {task.Name} <br />
+            <strong>Descripción:</strong> {task.Desc}
+            <button onClick={() => handleDeleteTask(index)}>Eliminar</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <App></App>
+  </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
